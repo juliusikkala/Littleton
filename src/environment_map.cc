355485d7d7591412271043717df60cc6e55e72f2
Julius Ikkala <julius.ikkala@gmail.com>
2018-03-31 17:28:58 +0300

    Add environment map class

---
+++ src/environment_map.cc
@@ -0,0 +1,128 @@
+#include "environment_map.hh"
+#include <cstring>
+
+environment_map::environment_map(context& ctx)
+: texture(ctx)
+{
+}
+
+environment_map::environment_map(
+    context& ctx,
+    const std::string& path,
+    bool srgb
+): texture(ctx, path, srgb, GL_TEXTURE_CUBE_MAP)
+{
+}
+
+environment_map::environment_map(
+    context& ctx,
+    glm::uvec2 size,
+    GLint format,
+    GLenum type,
+    const void* data
+): texture(ctx, size, format, type, 0, GL_TEXTURE_CUBE_MAP, data)
+{
+}
+
+environment_map::environment_map(environment_map&& other)
+: texture(std::move(other))
+{
+}
+
+environment_map::~environment_map() {}
+
+class file_environment_map: public environment_map
+{
+public:
+    file_environment_map(
+        context& ctx,
+        const std::string& path,
+        bool srgb
+    ): environment_map(ctx), srgb(srgb), path(path) {}
+
+protected:
+    void load_impl() const override
+    {
+        basic_load(path, srgb, GL_TEXTURE_CUBE_MAP);
+    }
+
+    void unload_impl() const override
+    {
+        basic_unload();
+    }
+
+private:
+    bool srgb;
+    std::string path;
+};
+
+environment_map* environment_map::create(
+    context& ctx,
+    const std::string& path,
+    bool srgb
+){
+    return new file_environment_map(ctx, path, srgb);
+}
+
+class data_environment_map: public environment_map
+{
+public:
+    data_environment_map(
+        context& ctx,
+        glm::uvec2 size,
+        GLint internal_format,
+        GLenum type,
+        size_t data_size,
+        const void* data
+    ): environment_map(ctx), size(size)
+    {
+        this->internal_format = internal_format;
+        this->type = type;
+
+        if(data)
+        {
+            this->data = new uint8_t[data_size];
+            memcpy(this->data, data, data_size);
+        }
+    }
+
+    ~data_environment_map()
+    {
+        if(data) delete [] (uint8_t*)data;
+    }
+
+protected:
+    void load_impl() const override
+    {
+        basic_load(
+            size,
+            internal_format,
+            type,
+            0,
+            GL_TEXTURE_CUBE_MAP,
+            data
+        );
+    }
+
+    void unload_impl() const override
+    {
+        basic_unload();
+    }
+
+private:
+    glm::uvec2 size;
+    void* data;
+};
+
+environment_map* environment_map::create(
+    context& ctx,
+    glm::uvec2 size,
+    GLint internal_format,
+    GLenum type,
+    size_t data_size,
+    const void* data
+){
+    return new data_environment_map(
+        ctx, size, internal_format, type, data_size, data
+    );
+}
---
+++ src/environment_map.hh
@@ -0,0 +1,47 @@
+#ifndef ENVIRONMENT_MAP_HH
+#define ENVIRONMENT_MAP_HH
+#include "glheaders.hh"
+#include "texture.hh"
+#include <string>
+#include <glm/glm.hpp>
+
+class environment_map: public texture
+{
+public:
+    environment_map(
+        context& ctx,
+        const std::string& path,
+        bool srgb = false
+    );
+
+    environment_map(
+        context& ctx,
+        glm::uvec2 size,
+        GLint format,
+        GLenum type,
+        const void* data = nullptr
+    );
+
+    environment_map(const environment_map& other) = delete;
+    environment_map(environment_map&& other);
+    ~environment_map();
+
+    static environment_map* create(
+        context& ctx,
+        const std::string& path,
+        bool srgb = false
+    );
+
+    static environment_map* create(
+        context& ctx,
+        glm::uvec2 size,
+        GLint internal_format,
+        GLenum type,
+        size_t data_size = 0,
+        const void* data = nullptr
+    );
+protected:
+    environment_map(context& ctx);
+};
+
+#endif
