696d432f8f750d3621eef979df6d8f05e00606e1
Julius Ikkala <julius.ikkala@gmail.com>
2018-07-04 00:19:30 +0300

    Add apply_sg method

---
+++ include/littleton/method/apply_sg.hh
@@ -0,0 +1,74 @@
+/*
+    Copyright 2018 Julius Ikkala
+
+    This file is part of Littleton.
+
+    Littleton is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Littleton is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Littleton.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef LT_METHOD_APPLY_SG_HH
+#define LT_METHOD_APPLY_SG_HH
+#include "../api.hh"
+#include "../pipeline.hh"
+#include "../primitive.hh"
+#include "../sampler.hh"
+#include "../framebuffer.hh"
+#include "../doublebuffer.hh"
+#include "../resource.hh"
+#include "../stencil_handler.hh"
+#include "shadow_method.hh"
+#include <memory>
+
+namespace lt
+{
+
+class gbuffer;
+class resource_pool;
+class shader;
+
+}
+
+namespace lt::method
+{
+
+class LT_API apply_sg: public target_method, public stencil_handler
+{
+public:
+    apply_sg(
+        render_target& target,
+        gbuffer& buf,
+        resource_pool& pool,
+        render_scene* scene
+    );
+
+    void set_scene(render_scene* scene);
+    render_scene* get_scene() const;
+
+    void execute() override;
+
+    std::string get_name() const override;
+
+private:
+    gbuffer* buf;
+
+    shader* sg_shader;
+    render_scene* scene;
+
+    const primitive& cube;
+    const sampler& fb_sampler;
+    const sampler& linear_sampler;
+};
+
+} // namespace lt::method
+
+#endif
---
+++ src/method/apply_sg.cc
@@ -0,0 +1,124 @@
+/*
+    Copyright 2018 Julius Ikkala
+
+    This file is part of Littleton.
+
+    Littleton is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Littleton is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Littleton.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "apply_sg.hh"
+#include "shader.hh"
+#include "math.hh"
+#include "gbuffer.hh"
+#include "resource_pool.hh"
+#include "camera.hh"
+#include "scene.hh"
+#include "common_resources.hh"
+#include "spherical_gaussians.hh"
+#include <algorithm>
+
+namespace lt::method
+{
+
+apply_sg::apply_sg(
+    render_target& target,
+    gbuffer& buf,
+    resource_pool& pool,
+    render_scene* scene
+):  target_method(target),
+    stencil_handler(GL_NOTEQUAL, 1<<7, 1<<7),
+    buf(&buf),
+    sg_shader(pool.get_shader(
+        shader::path{"generic.vert", "sao/apply_sg.frag"}, {}
+    )),
+    scene(scene),
+    cube(common::ensure_cube_primitive(pool)),
+    fb_sampler(common::ensure_framebuffer_sampler(pool)),
+    linear_sampler(common::ensure_linear_sampler(pool))
+{
+}
+
+void apply_sg::set_scene(render_scene* scene)
+{
+    this->scene = scene;
+}
+
+render_scene* apply_sg::get_scene() const
+{
+    return scene;
+}
+
+void apply_sg::execute()
+{
+    target_method::execute();
+
+    if(!sg_shader || !scene)
+        return;
+
+    camera* cam = scene->get_camera();
+    if(!cam) return;
+
+    glDisable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_ONE, GL_ONE);
+
+    glm::mat4 p = cam->get_projection();
+    glm::mat4 v = glm::inverse(cam->get_global_transform());
+    glm::mat4 vp = p * v;
+
+    std::vector<sg_group*> groups_by_density(scene->get_sg_groups());
+    std::sort(
+        groups_by_density.begin(),
+        groups_by_density.end(),
+        [](sg_group* a, sg_group* b){
+            return a->get_density() > b->get_density();
+        }
+    );
+
+    buf->bind_textures(fb_sampler);
+    buf->set_uniforms(sg_shader);
+    sg_shader->set("projection_info", cam->get_projection_info());
+    sg_shader->set("clip_info", cam->get_clip_info());
+
+    for(sg_group* group: groups_by_density)
+    {
+        const std::vector<sg_lobe>& lobes = group->get_lobes();
+
+        glm::mat4 m = group->get_global_transform();
+        glm::mat4 mv = v * m;
+
+        sg_shader->set("m", glm::mat4(1.0));
+        sg_shader->set("inv_m", glm::inverseTranspose(mv));
+        sg_shader->set("mvp", vp * m);
+
+        stencil_cull();
+        for(unsigned i = 0; i < lobes.size(); ++i)
+        {
+            if(i == lobes.size()-1)
+                stencil_draw_cull();
+            sg_shader->set(
+                "sg_lobe",
+                linear_sampler.bind(group->get_amplitudes(i), 6)
+            );
+            cube.draw();
+        }
+    }
+}
+
+std::string apply_sg::get_name() const 
+{
+    return "apply_sg";
+}
+
+} // namespace lt::method
